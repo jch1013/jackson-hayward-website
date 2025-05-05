919cf24676a1c10e5707a379f9eddc99524642fb
Jackson <jchayward1@gmail.com>
2025-05-04 22:03:20 -0700

    update project wheel with test projects and js functionality

--- main/views.py
+++ main/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import TimelineItem
+from .models import Project
 
 
 
@@ -10,13 +11,14 @@ def home(request):
 
 # Projects view
 def projects(request):
-    return render(request, 'projects.html')
+    project_items = Project.objects.all()
+    return render(request, 'projects.html', {'project_items' : project_items})
 
-# About me view
+# Experience me view
 def experience(request):
     timeline_items = TimelineItem.objects.all()
     return render(request, 'experiences.html', {'timeline_items': timeline_items})
 
-# Experience view
+# About me view
 def about_me(request):
     return render(request, 'about_me.html')
